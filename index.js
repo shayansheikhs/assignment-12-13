1045256b8397be2a0edc2310150273a7027c7b06
shayansheikhs <shayanshaikh530@gmail.com>
2023-08-28 23:50:05 +0500

    coommit

---
+++ index.js
@@ -0,0 +1,93 @@
+var A = "60";
+/*
+var Z = "90";
+var a= "95";
+var z = "122";
+var check=+prompt("give me a number upper case and lowercase") 
+if(   check>=A && check  <= Z){
+    document.write(" uppercase letter")
+}else if (  check>=a && check<= z) {
+    document.write("lowercase")
+} else {
+    document.write('are equal')
+}*/
+/*
+var usernumber  =+prompt( "give me a first number")
+ var usernumber  =+prompt( "give me a  second number")
+ var num1= 10
+ var num2= 10
+ if (parseInt( num1 ,10)>( num2 ,10) ){
+     document.write( "<br> The largest of " + num1+ "and"  +num2 +"is" + num1+ "." ) 
+ } else 
+ if(parseInt(num2,10) > parseInt( num1,10)) {
+     document.write("  <br>The largest of" + num2 + "and"  + num1 +"is"+ num2 +"."  )
+ }
+ else{
+     document.write(" <br>The value  " + num1 + "and"+ num2 + "are equal")
+ }*/
+
+/*
+var positive = 1
+var negative = -0
+var check =+prompt(  "give me a positive or negative integer")
+if( check  >= positive){
+document.write("Postive integer") 
+} else if( check <= negative){
+   document.write("Negative  integer")
+} */
+/*
+var vowel = "aeiou"
+var check =prompt( "Give me a alphabet")
+if(check ===vowel){
+    document.write(" <br>True")
+}else {
+    document.write("<br> False")
+}*/
+/*
+ var password = 12345678
+var  userpassword =+prompt( "  Please enter your password")
+if(   userpassword === password){
+    document.write("Correct password")
+}else{
+    document.write("Incorrect password")
+}*/
+
+/*
+var greeting;
+var hour = 13;
+if (hour < 18) {
+greeting = "Good day";
+}else{
+greeting = "Good evening";
+}*/
+
+var fix = 0
+var time1 = 1200
+var time2 = 1700
+var time3 = 1900
+var time4 = 2100
+var time4 = 2359
+var insert = +prompt("please enter a insert time")
+if (insert >= fix && insert <= time1) {
+    document.write(" Good Morning")
+} else if (insert >= time1 && insert <= time2) {
+    document.write(" Good aftenoon")
+}
+ else if (insert >= time2 && insert <= time3) {
+    document.write(" Good evening")
+}
+ else if (insert >= time3 && insert <= time4) {
+    document.write(" Good night")
+}
+
+
+
+
+
+
+
+
+
+
+
+
